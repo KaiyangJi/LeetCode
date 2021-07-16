4b99ff93d31a019a01318277ff93f6b1c5551428
jikaiyang <ky.ji@idiaoyan.com>
2021-07-16 18:05:41 +0800

    feat: 数组转tree

---
+++ js/1.js
@@ -0,0 +1,64 @@
+/**
+ * let arr = [
+    {id: 1, name: '部门1', pid: 0},
+    {id: 2, name: '部门2', pid: 1},
+    {id: 3, name: '部门3', pid: 1},
+    {id: 4, name: '部门4', pid: 3},
+    {id: 5, name: '部门5', pid: 4},
+]
+
+转化为
+
+
+[
+    {
+        "id": 1,
+        "name": "部门1",
+        "pid": 0,
+        "children": [
+            {
+                "id": 2,
+                "name": "部门2",
+                "pid": 1,
+                "children": []
+            },
+            {
+                "id": 3,
+                "name": "部门3",
+                "pid": 1,
+                "children": [
+                    // 结果 ,,,
+                ]
+            }
+        ]
+    }
+]
+
+ */
+
+let arr = [
+    { id: 1, name: '部门1', pid: 0 },
+    { id: 2, name: '部门2', pid: 1 },
+    { id: 3, name: '部门3', pid: 1 },
+    { id: 4, name: '部门4', pid: 3 },
+    { id: 5, name: '部门5', pid: 4 },
+];
+function transArrToTree(arr) {
+    function pushChildren(obj) {
+        obj.children = obj.children || [];
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].pid === obj.id) {
+                obj.children.push(pushChildren(arr[i]));
+            }
+        }
+
+        console.log('obj', obj);
+
+        return obj;
+    }
+
+    const result = pushChildren(arr[0]);
+    return result;
+}
+
+transArrToTree(arr);
